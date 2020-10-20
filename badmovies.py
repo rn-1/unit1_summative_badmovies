1cc6ca975d27a35471c2f27aaf78e47cfda9e05a
rn-1 <69952976+rn-1@users.noreply.github.com>
2020-10-20 15:07:44 +0530

    A new version of badmovies
    This holds the information and can tell you about various movies. More work can be done to improve it, however.

--- badmovies.py
+++ badmovies.py
@@ -1,67 +1,87 @@
 # god has left us and this file this file killed him.
 import requests as r
 from bs4 import BeautifulSoup as BS
-url = "https://www.rottentomatoes.com/browse/dvd-streaming-all?minTomato=0&maxTomato=10&services=amazon;hbo_go;itunes;netflix_iw;vudu;amazon_prime;fandango_now&genres=1;2;4;5;6;8;9;10;11;13;18;14&sortBy=release"
+url = "https://www.rottentomatoes.com/browse/dvd-streaming-all?minTomato=0&maxTomato=9&services=amazon;hbo_go;itunes;netflix_iw;vudu;amazon_prime;fandango_now&genres=1;2;4;5;6;8;9;10;11;13;18;14&sortBy=release"
 soup = r.get(url).content
 data = BS(soup,"html.parser")
-class movie():
-    def __init__(self):
+class movie(): #this is a class that pretty much does nothing other than hold all these functions. Which sucks, but oh well.
+    def __init__(self): #get the data we want. Clean it up and prep it for the rest of the class to use. 
         mov = data.select(".body_main")
-        return mov
-    def rating(self,data):
-        ratings = []
-        souped = BS(str(data),"html.parser")
-        r = souped.find_all("span",class_="tMeterScore")
-        for i in r:
-            hell = BS(str(i),"html.parser")
-            s = hell.span.contents
-            #print(s[0].replace('%',""))
-            x = s[0].replace('%',"")
-            ratings.append(int(x))
-        return ratings
-    def movienames(self,data):
-        souped = BS(str(data),"html.parser")
-        mn = souped.find_all("h3",class_="movieTitle")
-        names = []
-        for n in mn:
-            pain = BS(str(n),"html.parser")
-            b = pain.h3.contents
-            names.append(b)
-        return names
-    def actors(self,data):
-        souped = BS(str(data),"html.parser")
-        aa = souped.find_all("p",class_="actors")
-        names = []
-        for n in aa:
-            pain = BS(str(n),"html.parser")
-            b = pain.p.contents
-            names.append(b)
-        return names
-    def synopsis(self,data):
-        souped = BS(str(data),"html.parser")
-        aa = souped.find_all("span",class_="consensus-text")
-        names = []
-        for n in aa:
-            pain = BS(str(n),"html.parser")
-            b = pain.span.contents
-            names.append(b)
-        return names
-    def summary(self,mn,r,a,s):
+        parse = BS(str(mov),"html.parser")
+        scripts = parse.find_all("script")
+        check = "[{\"id\""
+        thing = []
+        for sc in scripts:
+            if check in str(sc):
+                #print(sc)
+                thing.append(str(sc).strip())
+        thing = thing[0].split('\n')
+        for i in thing:
+            if check in i:
+                thing = [i]
+        return thing
+    def findGeneral(self,data,check): #A function used to find most of the information we want. This doesn't include actors, as this first split would mess with the script.
+        data = data[0].split(',\"') # Separating it by commas, this makes it far easier to find the stuff.
+        result = []
+        for d in data:
+            if check in d:
+                result.append(d.strip(check))
+        return result
+    def actors(self,data): #Find the actors. This is similar to findGeneral, but somewhat different.
+        data = data[0].split(',\"')
+        result = []
+        check = "actors\":"
+        for d in data:
+            if check in d:
+                index = data.index(d)
+                temp = data[index:index+1]
+                temp = temp[0].split('},{')
+                for n in temp:
+                    if check in n:
+                        temp = n.strip(check)
+                result.append(temp)
+        return result
+    def summary(self,mn,r,a,s): #Compile this stuff into a dict.
         summ = {}
-        for i in names:
-            pass
-        return None
-        pass
+        elvisgone = False
+        for i in range(len(mn)):
+            if mn[i] == "Elvis from Outer Spac":
+                summ[mn[i]] = r[i],"Unlisted",s[i]
+                elvisgone = True
+            elif elvisgone == True:
+                summ[mn[i]] = r[i],a[i-1],s[i]
+            else:
+                summ[mn[i]] = r[i],a[i],s[i]
+        return summ
         
-#main
-stuff = movie.__init__(movie)
-names = movie.movienames(movie,stuff)
-ratings = movie.rating(movie,stuff)
-actors = movie.actors(movie,stuff)
-syn = movie.synopsis(movie,stuff)
-full = movie.summary(movie,names,ratings,actors,syn)
+#main. Doesn't really mean much, just a place where all the values are put in the right scope.
+stuff = movie.__init__(movie) #Initialise the stuff, get the data.
+movienames = movie.findGeneral(movie,stuff,"title\":") #Search the block of text for data pieces with the desired tag.
+ratings = movie.findGeneral(movie,stuff,"tomatoScore\":") #Ditto
+actors = movie.actors(movie,stuff) #Actors is different because of the website's format.
+syn = movie.findGeneral(movie,stuff,"synopsis\":") #Finds the synopsis string, makes a list of them.
+full = movie.summary(movie,movienames,ratings,actors,syn) #Takes all previous data and creates a dictionary with {names:ratings,actors,syn}.
+#print commands, for debugging mainly.
+
 #print(stuff)
-print(names)
-print(ratings)
-print(actors)
-print(syn)
+#print(movienames)
+#print(ratings)
+#print(actors)
+#print(syn)
+#print(full)
+print("""This is a discount rotten tomatoes.
+Note that movie descriptions are ordered as follows:
+Rating, Key actors, and Synopsis.
+Inputs are sanitized, but avoid the '' around titles. Below are a list of available movies to look at:""")
+print(movienames)
+while True:
+    inp = str(input("Which movie would you like to view?:"))
+    inp = inp.strip('\"')
+    inp = inp.strip()
+    print(inp)
+    try:
+        print(full[inp])
+        continue
+    except:
+        print("That movie is not listed, or there might be a typo:",inp)
+        continue
